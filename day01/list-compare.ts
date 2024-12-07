b6a853ce54c84b336e7a7224fb91bc136c01903b
Tom Braul <tobr772@gmail.com>
2024-12-07 19:27:19 +0100

    Refactor variable names in list-compare.ts for clarity

--- day01/list-compare.ts
+++ day01/list-compare.ts
@@ -6,7 +6,8 @@ let [list1, list2]: number[][] = await readInput(INPUT_FILE_PATH);
 list1 = list1.sort();
 list2 = list2.sort();
 
-const result = list1.reduce((sum, listValue, index) => {
-    return sum + Math.abs(listValue - list2[index]);
+const result = list1.reduce((sum, list1Value, index) => {
+    const list2Value = list2[index];
+    return sum + Math.abs(list1Value - list2Value);
 },  0)
 console.log(result);
